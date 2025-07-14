ae1ec854eb5fc7452c28cb8cd120bd6bf930a6af
Okno Capital <leads@oknocapital.com.br>
2025-07-14 16:37:15 -0300

    Create staging.assertiva_localize_contatos.sql

---
+++ staging.assertiva_localize_contatos.sql
@@ -0,0 +1,14 @@
+CREATE TABLE staging.assertiva_localize_contatos (
+  doc              VARCHAR(18)     NOT NULL,  -- CPF ou CNPJ
+  nome             TEXT            NOT NULL,
+  contato          VARCHAR(100)    NOT NULL,
+  ordem            INT             NOT NULL,
+  tipo_contato     TEXT,
+  ultimo_contato   TIMESTAMPTZ,
+  relacao          TEXT,
+  nao_perturbe     BOOLEAN         NOT NULL DEFAULT null,
+  whatsapp         BOOLEAN         NOT NULL DEFAULT null,
+
+  CONSTRAINT uq_escavador_parte_contatos_contato_doc
+    UNIQUE (contato, doc)
+);
